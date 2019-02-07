904dc6bc330b9db4cd314efdfdd8e25967e2cdb6
Shriram <shriramsinbox@gmail.com>
2019-02-07 00:37:00 -0600

    delete working

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { ui } from './ui';
 document.addEventListener('DOMContentLoaded', getPosts);
 document.querySelector('.post-submit').addEventListener('click',submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click',deletePost);
 
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -29,4 +31,21 @@ function submitPost() {
         getPosts();
       })
       .catch(err => console.log(err));
+}
+
+function deletePost(e) {
+  const post = e.target;
+
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+          .then( data => {
+            ui.showAlert('Post deleted', 'alert alert-success');
+            getPosts();
+          })
+          .catch(err => console.log(err));
+    }
+  }
+  e.preventDefault();
 }
